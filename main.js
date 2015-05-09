5a13e13bf00356be0fcf374f86d61d1050caf5ce
David Paz Brea <dpaz1993@gmail.com>
2015-05-09 20:46:35 +0200

    Añadida funcion para buscar las fotos en flickr pero busca lo que quiere

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 $(document).ready(function(){
 
+    //Creo el mapa lo centro en el 0,0 y le doy una capa de teselas.
     map = L.map("map").setView([0,0], 2);
     L.tileLayer(' http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png').addTo(map);
-    console.log("acabo");
-    console.log(map);
+    
+
+    fotosflikr("madrid")
+
+    function fotosflikr(tag){
+       
+        $.getJSON("https://api.flickr.com/services/feeds/photos_public.gne?&tags="+tag+"&tagmode=any&format=json&jsoncallback=?",
+            function(data){
+                data = data.items.splice(0,5)
+                console.log(data);
+                for(i=0; i<5 ; i++){
+                    $("#car"+i).attr("src",data[i].media.m)
+                }
+                    
+               
+        });
+    }
+
+
 });
